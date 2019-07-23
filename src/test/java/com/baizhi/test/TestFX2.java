50215c2b4effa6fef0147c3ae4fa3e647baef924
MyGit <1132975708@qq.com>
2019-07-23 19:23:31 +0800

    删除掉了一些无用信息

--- src/test/java/com/baizhi/test/TestFX2.java
+++ src/test/java/com/baizhi/test/TestFX2.java
@@ -13,14 +13,5 @@ public class TestFX2 {
     public static void main(String[] args) {
         TestFX2 tf = new TestFX2();
         tf.test2();
-        System.out.println("+++++++++++++");
-         System.out.println("+++++++++++++");
-         System.out.println("+++++++++++++");
-        System.out.println("**********");
-        System.out.println("**********");
-        System.out.println("&&&&&&&&&&");
-        System.out.println("=============");
-        System.out.println("&======&");
-        System.out.println("&&%%%%%%%&");
     }
 }
