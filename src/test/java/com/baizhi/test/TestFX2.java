6e4c86d0f640aaba6ca193bfbc0b04d4de5f406d
MyGit <1132975708@qq.com>
2019-07-23 16:30:38 +0800

    添加了几行代码而已

--- src/test/java/com/baizhi/test/TestFX2.java
+++ src/test/java/com/baizhi/test/TestFX2.java
@@ -19,5 +19,8 @@ public class TestFX2 {
         System.out.println("**********");
         System.out.println("**********");
         System.out.println("&&&&&&&&&&");
+        System.out.println("=============");
+        System.out.println("&======&");
+        System.out.println("&&%%%%%%%&");
     }
 }
