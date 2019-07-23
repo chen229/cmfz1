f8b414c16c4f22f4bfc9ca23c3ed442fe9e05c18
chen <53084576+chen229@users.noreply.github.com>
2019-07-23 14:52:14 +0800

    Update TestFX2.java

--- src/test/java/com/baizhi/test/TestFX2.java
+++ src/test/java/com/baizhi/test/TestFX2.java
@@ -6,12 +6,16 @@ public class TestFX2 {
 
     }
     public static void test2(){
-        test1();
+       
         System.out.println("this is test2");
     }
 
     public static void main(String[] args) {
         TestFX2 tf = new TestFX2();
         tf.test2();
+        System.out.println("+++++++++++++");
+         System.out.println("+++++++++++++");
+         System.out.println("+++++++++++++");
+        
     }
 }
